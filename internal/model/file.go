c47e03ae73f0cce7b35525e1c35689db554417e3
maocili <maocili@163.com>
2020-04-24 17:45:52 +0800

    增加两个数据表 file_data user_file

---
+++ internal/model/file.go
@@ -0,0 +1,16 @@
+package model
+
+type FileData struct {
+	// Id       int    `xorm:"int(11)" form:"id" json:"id"`
+	FileSha1 string `xorm:"pk char(40)" form:"file_sha1" json:"file_sha1"`
+	FileSize string `xorm :"varchar(256)" form:"file_size" json:"file_size"`
+	FileAddr string `xorm:"varchar(1024)" form:"file_addr" json:"file_addr"`
+}
+
+type UserFile struct {
+	// Id       int    `xorm:"int(11)" form :"id" json:"id"`
+	Uid      string `xorm:"varchar(256)" form:"uid" json:"uid"`
+	FileSha1 string `xorm:"char(40)" form:"file_sha1" json:"file_sha1"`
+	FileName string `xorm:"varchar(256)" form:"file_name" json:"file_name"`
+	UserPath string `xorm:"char(40)" form:"user_path" json:"user_path"`
+}
