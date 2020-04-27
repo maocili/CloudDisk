b691a2acac6da30bd4ff11cb03a0d48249266e7c
maocili <maocili@163.com>
2020-04-27 12:07:56 +0800

    + 数据库获取文件目录、用户文件名、文件信息

--- internal/dao/file.go
+++ internal/dao/file.go
@@ -3,6 +3,7 @@ package dao
 import (
 	"CloudDisk/internal/model"
 	"CloudDisk/tools/mysql"
+	"fmt"
 )
 
 func NewFile(filedata model.FileData) error {
@@ -25,4 +26,29 @@ func IsexistFileHash(filehash string) (bool, error) {
 		FileSha1: filehash,
 	})
 	return has, err
+}
+
+func GetFileList(uid string, zones string) ([]model.UserFile, error) {
+	var userFile []model.UserFile
+
+	err := mysql.DB.Table("user_file").Select("*").Where("uid = ? and path_id=?", uid, zones).Find(&userFile)
+
+	return userFile, err
+}
+
+func GetUserFileName(uid, filehash string) (string, error) {
+
+	var userFile model.UserFile
+	var fileName string
+	has, err := mysql.DB.Table(&userFile).Where("uid = ? and file_sha1 = ?", uid, filehash).Cols("file_name").Get(&fileName)
+	fmt.Println(has, fileName)
+	return fileName, err
+}
+
+func GetFileData(filehash string) (model.FileData, error) {
+	var fileData []model.FileData
+
+	err := mysql.DB.Table("file_data").Select("*").Where("file_sha1=?", filehash).Find(&fileData)
+
+	return fileData[0], err
 }
